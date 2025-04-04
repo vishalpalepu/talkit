be8696e4a542e83f7e0f768de773429dc88b00fd
vishalpalepu <vishal.cinpalepu@gmail.com>
2025-04-04 22:44:21 +0530

    login and authentication

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { register } from "../controller/auth.contoller.js";
+import { register, login } from "../controller/auth.contoller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const Router = express.Router();
 
-// Router.post("/login");
-// Router.post("/logout");
+Router.post("/login", login);
+Router.get("/logout", protectRoute, (req, res) => {
+  return res.send("middleware successful");
+});
 Router.post("/register", register);
 // Router.put("/update-profile"); // needs to be secure channel (middleware)
 // Router.get("/check"); // needs a secure channel (middleware)
