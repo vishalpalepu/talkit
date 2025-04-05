03205bcb31185f475e96b10992d597111587d4ee
vishalpalepu <vishal.cinpalepu@gmail.com>
2025-04-05 10:27:07 +0530

    logout,updateprofile get profile

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,15 +1,19 @@
 import express from "express";
-import { register, login } from "../controller/auth.contoller.js";
+import {
+  register,
+  login,
+  logout,
+  updateProfile,
+  getUserProfile,
+} from "../controller/auth.contoller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const Router = express.Router();
 
 Router.post("/login", login);
-Router.get("/logout", protectRoute, (req, res) => {
-  return res.send("middleware successful");
-});
+Router.post("/logout", logout);
 Router.post("/register", register);
-// Router.put("/update-profile"); // needs to be secure channel (middleware)
-// Router.get("/check"); // needs a secure channel (middleware)
+Router.put("/updateProfile", protectRoute, updateProfile); // needs to be secure channel (middleware)
+Router.get("/check", protectRoute, getUserProfile); // needs a secure channel (middleware)
 
 export default Router;
