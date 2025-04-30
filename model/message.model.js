4890c268a3a748e29dcc88047a935cf495d4c3c4
vishalpalepu <vishal.cinpalepu@gmail.com>
2025-04-30 14:51:21 +0530

    function to delete image extract pubID uploading (reducing the size)

--- model/message.model.js
+++ model/message.model.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const messageSchema = mongoose.Schema({
-  senderID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const messageSchema = mongoose.Schema(
+  {
+    senderID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
   },
-  receiverID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  text: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Message = mongoose.model("message", messageSchema);
 
